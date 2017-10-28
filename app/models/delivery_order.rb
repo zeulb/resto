f397022e62eb0c812bc932aacce8650c2f01c376
Stefano Chiesa <base278@gmail.com>
2017-10-28 22:23:10 +0800

    Generate DeliveryOrder model

---
+++ app/models/delivery_order.rb
@@ -0,0 +1,2 @@
+class DeliveryOrder < ApplicationRecord
+end
---
+++ db/migrate/20171028141214_create_delivery_orders.rb
@@ -0,0 +1,12 @@
+class CreateDeliveryOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :delivery_orders do |t|
+      t.string :order_id
+      t.datetime :serving_datetime
+
+      t.timestamps
+    end
+  end
+
+  add_index :delivery_orders, :order_id, unique: true
+end
