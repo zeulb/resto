c2e2d7e81c405a531ca6ddf86d6710695fb4291f
Stefano Chiesa <base278@gmail.com>
2017-10-29 01:41:32 +0800

    Add order show api

--- app/controllers/delivery_orders_controller.rb
+++ app/controllers/delivery_orders_controller.rb
@@ -3,4 +3,20 @@ class DeliveryOrdersController < ApplicationController
     @orders = DeliveryOrder.all
     json_response(@orders)
   end
+
+  def show
+    @order = DeliveryOrder.where(["order_id = ?", params[:id]]).first
+    json_response({
+      order_id: @order.id,
+      delivery_date: @order.serving_datetime.to_date,
+      delivery_time: @order.serving_datetime.strftime("%H:%M"),
+      order_items: @order.order_items.map { |order_item|
+        {
+          name: order_item.meal.name,
+          quantity: order_item.quantity,
+          total_price: order_item.quantity * order_item.unit_price
+        }
+      }
+    })
+  end
 end
