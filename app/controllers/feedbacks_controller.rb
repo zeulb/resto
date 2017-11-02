52954f01524598abcd318f6b36297df98413f660
Stefano Chiesa <base278@gmail.com>
2017-11-02 23:54:46 +0800

    Create feedback api

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,6 +1,7 @@
 class FeedbacksController < ApplicationController
+  before_action :set_order
+
   def index
-    @order = DeliveryOrder.where(order_id: params[:delivery_order_id]).first!
     json_response(
       collect_feedbacks(@order).map { |feedback|
         {
@@ -11,7 +12,42 @@ class FeedbacksController < ApplicationController
       })
   end
 
+  def create
+    writable_ratables = [
+      @order,
+      *@order.order_items
+    ];
+    @order.transaction do
+      params[:feedbacks].each { |ratable_params|
+        ratable = get_ratable(ratable_params[:ratable_type], ratable_params[:ratable_id])
+        if writable_ratables.include? ratable
+          ratable.create_feedback!({
+            rating: ratable_params[:rating],
+            comment: ratable_params[:comment]
+          })
+        else
+          raise ArgumentError.new('Invalid ratable_id or ratable_type')  
+        end
+      }
+    end
+    json_response({ status: "OK" }, :created)
+  end
+
   private
+
+  def set_order
+    @order = DeliveryOrder.where(order_id: params[:delivery_order_id]).first!
+  end
+
+  def get_ratable(ratable_type, ratable_id)
+    if ratable_type == "DeliveryOrder"
+      DeliveryOrder.find(ratable_id)
+    elsif ratable_type == "OrderItem"
+      OrderItem.find(ratable_id)
+    else
+      nil
+    end
+  end
 
   def collect_feedbacks(order)
     if order.feedback.nil?
