bda3487b68af69d09854e82a129ab05b37e17743
Stefano Chiesa <base278@gmail.com>
2017-11-04 04:03:55 +0800

    Add ratable id to GET orders api

--- app/controllers/delivery_orders_controller.rb
+++ app/controllers/delivery_orders_controller.rb
@@ -19,12 +19,14 @@ class DeliveryOrdersController < ApplicationController
     
     {
       order_id: order.order_id,
+      ratable_id: order.id,
       delivery_date: serving_start_datetime.to_date,
       delivery_time: serving_start_datetime.strftime("%H:%M") + "-" + serving_end_datetime.strftime("%H:%M"),
       feedback_submitted: !order.feedback.nil?,
       order_items: order.order_items.map { |order_item|
         {
           name: order_item.meal.name,
+          ratable_id: order_item.id,
           quantity: order_item.quantity,
           total_price: order_item.quantity * order_item.unit_price
         }
