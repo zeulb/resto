9d312d6b41ce01e0718e4fc0f47176bb42d967ef
Stefano Chiesa <base278@gmail.com>
2017-11-03 19:38:38 +0800

    Add authentication check to whole api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authorization = request.headers['Authorization']
+    unless authorization.nil?
+      words = authorization.split(' ')
+      if words.length == 2 && words[0] == 'Bearer'
+        authenticated_token = Token.where(token: words[1]).first
+        return unless authenticated_token.nil?
+      end
+    end
+    json_response({ status: "UNAUTHENTICATED" }, :unauthorized)
+  end
 end
